62b0418ece21f260a95e552948a304b2f9719aca
Filip Rurak <filip.rurak@movecloser.pl>
2026-03-23 13:14:29 +0100

    Cleanup

--- src/views/SlideView.vue
+++ src/views/SlideView.vue
@@ -10,19 +10,25 @@
     </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue"
-import type { ISlide } from "@/definitions/slide/Slide"
-import SlideList from "@/components/slideList/SlideList.vue"
-import SlidePreview from "@/components/slidePreview/SlidePreview.vue"
-import SlideConfig from "@/components/slideConfig/SlideConfig.vue"
-import aiOutput from "@/defaults/ai/ai-output.json"
+<script
+    setup
+    lang="ts"
+>
+import { ref } from "vue";
+import type { ISlide } from "@/definitions/slide/Slide";
+import SlideList from "@/components/slideList/SlideList.vue";
+import SlidePreview from "@/components/slidePreview/SlidePreview.vue";
+import SlideConfig from "@/components/slideConfig/SlideConfig.vue";
+import aiOutput from "@/defaults/ai/ai-output.json";
 
-const activeIndex = ref<number>(0)
-const slides = ref<ISlide[]>(aiOutput.slides as ISlide[])
+const activeIndex = ref<number>(0);
+const slides = ref<ISlide[]>(aiOutput.slides as ISlide[]);
 </script>
 
-<style scoped lang="scss">
+<style
+    scoped
+    lang="scss"
+>
 .slide-view {
     display: flex;
     height: 100vh;
