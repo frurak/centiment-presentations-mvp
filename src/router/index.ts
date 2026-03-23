87e57a95b55e36bbd0b020f2377667997ce66209
Filip Rurak <filip.rurak@movecloser.pl>
2026-03-23 13:16:57 +0100

    deployment prep

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import SlideView from "@/views/SlideView.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/slide",
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { fileURLToPath, URL } from "node:url"
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import vueDevTools from "vite-plugin-vue-devtools"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
+    base: "/centiment-presentations-mvp/",
     plugins: [
         vue(),
         vueDevTools(),
@@ -14,4 +15,4 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-})
+});
